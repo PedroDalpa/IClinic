dedcc6a3faedc0d00e297d375622d3221f2773d1
phdgamer <pedro380dalpa@gmail.com>
2021-11-04 14:09:55 -0300

    Fix: add swagger on application

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import { AppError } from "./errors/AppError";
 import { routes } from "./routes";
+import SwaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./swagger.json";
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.use(
